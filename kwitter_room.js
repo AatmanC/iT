79d585295fb88e314154b1585e7dd329ca60ea93
AatmanC <74542657+AatmanC@users.noreply.github.com>
2021-05-22 14:26:38 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -39,7 +39,7 @@ getData();
 function redirectToRoomName(name){
       console.log(name);
       localStorage.setItem("room_name", name);
-        window.location = "kwitter_page.html";
+
 }
 
 function logout(){
